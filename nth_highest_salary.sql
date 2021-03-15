206b9eb74f4e1a171ce7d97b02659f271c13679d
Subhayan Ghosh <subhayan.learn@gmail.com>
2021-03-15 15:21:03 +0530

    New ways to get nth highest sal

--- nth_highest_salary.sql
+++ nth_highest_salary.sql
@@ -1,4 +1,58 @@
---secondhighestsalary
+-- Normal approach
+SELECT
+   MAX(Salary)
+FROM
+   Employees
+WHERE
+   Salary NOT IN
+   (
+      SELECT
+         MAX(Salary)
+      FROM
+         Employees
+   )
+;
+
+SELECT *
+FROM
+   Employees
+ORDER BY
+   Salary DESC OFFSET 1 ROW
+FETCH NEXT 1 ROW ONLY
+;
+
+SELECT
+   MAX(Salary)
+FROM
+   Employees
+WHERE
+   Salary <
+   (
+      SELECT
+         MAX(Salary)
+      FROM
+         Employees
+   )
+;
+
+--second highest salary with CTE and DENSE RANK
+WITH T AS
+   (
+      SELECT *
+       , DENSE_RANK() OVER(ORDER BY Salary DESC) AS RNK
+      FROM
+         Employee
+   )
+SELECT
+   EMPNAME
+ , SAL
+FROM
+   T
+WHERE
+   RNK=2 -- Here 2 can be replaced by any number
+   
+
+--second highest salary without CTE
 SELECT
    emp.SecondHighestSalary
 FROM
@@ -13,6 +67,8 @@ FROM
 WHERE
    emp.rank = 2;
    
+ 
+-- Using an UDF
 CREATE FUNCTION getNthHighestSalary(N IN NUMBER) 
 	RETURN NUMBER IS
 	result NUMBER;
